bd5036310036db9100f0a31133cd87feec4ea745
Arsh Bhatti <A.Bhatti@walmartlabs.com>
2020-09-09 09:09:54 -0700

    Finished work experience

--- src/Components/WhatIDo.js
+++ src/Components/WhatIDo.js
@@ -1,58 +1,72 @@
-import React from 'react';
-import {Col, Row, Container} from "reactstrap";
+import React from "react";
+import { Col, Row, Container } from "reactstrap";
 
-import './Layout.css';
-
+import "./Layout.css";
 
 const WhatIDo = () => {
-    return (
-        <>
-            <Col md={6} sm={12} className="whatIDoGridLeft">
-                <Row>
-                    <img src="https://i.ibb.co/8Y2Yq0R/all-logo.png" className="webDevelopmentImage"/>
-                    <h3>
-                        WEB DEVELOPMENT
-                    </h3>
-                    <p>
-                        Using HTML, CSS, and JavaScript with pre-processors and build tools such as Sass and Grunt, 
-                        I have a passion for developing pixel-perfect websites and apps while maintaining a semantic, modular, and DRY code base.
-                    </p>
-                </Row>
-                <Row className="whatIDoGridBottomRow">
-                    <img src="https://i.ibb.co/8Y2Yq0R/all-logo.png" className="webDevelopmentImage"/>
-                    <h3>
-                        WEB DEVELOPMENT
-                    </h3>
-                    <p>
-                        Using HTML, CSS, and JavaScript with pre-processors and build tools such as Sass and Grunt, 
-                        I have a passion for developing pixel-perfect websites and apps while maintaining a semantic, modular, and DRY code base.
-                    </p>
-                </Row>
-            </Col>
-            <Col md={6} sm={12}>
-                 <Row>
-                    <img src="https://i.ibb.co/8Y2Yq0R/all-logo.png" className="webDevelopmentImage"/>
-                    <h3>
-                        WEB DEVELOPMENT
-                    </h3>
-                    <p>
-                        Using HTML, CSS, and JavaScript with pre-processors and build tools such as Sass and Grunt, 
-                        I have a passion for developing pixel-perfect websites and apps while maintaining a semantic, modular, and DRY code base.
-                    </p>
-                </Row>
-                 <Row className="whatIDoGridBottomRow">
-                    <img src="https://i.ibb.co/8Y2Yq0R/all-logo.png" className="webDevelopmentImage"/>
-                    <h3>
-                        WEB DEVELOPMENT
-                    </h3>
-                    <p>
-                        Using HTML, CSS, and JavaScript with pre-processors and build tools such as Sass and Grunt, 
-                        I have a passion for developing pixel-perfect websites and apps while maintaining a semantic, modular, and DRY code base.
-                    </p>
-                </Row>
-            </Col>
-        </>
-    );
+  return (
+    <>
+      <Col md={6} sm={12} className="whatIDoGridLeft">
+        <Row>
+          <img
+            src="https://i.ibb.co/8Y2Yq0R/all-logo.png"
+            alt="web-development"
+            className="webDevelopmentImage"
+          />
+          <h3>WEB DEVELOPMENT</h3>
+          <p>
+            Using HTML, CSS, and JavaScript,I have a passion for developing
+            pixel-perfect websites and apps while maintaining a semantic,
+            modular, and DRY code base.
+          </p>
+        </Row>
+        <Row className="whatIDoGridBottomRow">
+          <img
+            src="https://i.ibb.co/8Y2Yq0R/all-logo.png"
+            alt="web-development"
+            className="webDevelopmentImage"
+          />
+          <h3>WEB DEVELOPMENT</h3>
+          <p>
+            Using HTML, CSS, and JavaScript with pre-processors and build tools
+            such as Sass and Grunt, I have a passion for developing
+            pixel-perfect websites and apps while maintaining a semantic,
+            modular, and DRY code base.
+          </p>
+        </Row>
+      </Col>
+      <Col md={6} sm={12}>
+        <Row>
+          <img
+            src="https://i.ibb.co/8Y2Yq0R/all-logo.png"
+            alt="web-development"
+            className="webDevelopmentImage"
+          />
+          <h3>WEB DEVELOPMENT</h3>
+          <p>
+            Using HTML, CSS, and JavaScript with pre-processors and build tools
+            such as Sass and Grunt, I have a passion for developing
+            pixel-perfect websites and apps while maintaining a semantic,
+            modular, and DRY code base.
+          </p>
+        </Row>
+        <Row className="whatIDoGridBottomRow">
+          <img
+            src="https://i.ibb.co/8Y2Yq0R/all-logo.png"
+            alt="web-development"
+            className="webDevelopmentImage"
+          />
+          <h3>WEB DEVELOPMENT</h3>
+          <p>
+            Using HTML, CSS, and JavaScript with pre-processors and build tools
+            such as Sass and Grunt, I have a passion for developing
+            pixel-perfect websites and apps while maintaining a semantic,
+            modular, and DRY code base.
+          </p>
+        </Row>
+      </Col>
+    </>
+  );
 };
 
 export default WhatIDo;
--- src/Components/WorkExperience.js
+++ src/Components/WorkExperience.js
@@ -10,36 +10,56 @@ const WorkExperience = () => {
         style={{ color: "#e86971" }}
       >
         <h3>Front End - Full Stack Developer</h3>
-        <h4>@Walmart E-Commerce</h4>
+        <h4>@Walmart E-Commerce, Sunnyvale, CA</h4>
         <p>
-          Est incididunt sint eu minim dolore mollit velit velit commodo ex
-          nulla exercitation. Veniam velit adipisicing anim excepteur nostrud
-          magna nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-          reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis
-          Lorem est.
+          Tech Stack:{" "}
+          <b>
+            React, Redux, Javascript ES6,Cypress, Jest, Enzyme, Google Optimize,
+            Google Tag Manager, Lighthouse, React Testing Library,Webpack,
+            HTML5, CSS3,Lodash.
+          </b>
         </p>
         <p>
-          Est incididunt sint eu minim dolore mollit velit velit commodo ex
-          nulla exercitation. Veniam velit adipisicing anim excepteur nostrud
-          magna nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-          reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis
-          Lorem est.
+          As the sole Front-End Engineer, I lead the implementation of features
+          across 4 different codebases for 4 different Walmart Businesses in
+          Mexico.
         </p>
         <p>
-          Est incididunt sint eu minim dolore mollit velit velit commodo ex
-          nulla exercitation. Veniam velit adipisicing anim excepteur nostrud
-          magna nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
-          reprehenderit laborum fugiat dolore in elit. Adipisicing do qui duis
-          Lorem est.
+          Implemented features on the FE that have led to earnings/cost-savings
+          of over $100M Pesos/year for Walmart.
+        </p>
+        <p>
+          Collaborated with multiple teams using Agile methodology and put
+          people over processes and products.
+        </p>
+        <p>
+          Implemented unit and integration testing framework using Jest,Enzyme
+          and React Testing Library.
+        </p>
+        <p>
+          Did Performance evaluations of Client-Side Applications using Chrome
+          Lighthouse and made apps performant using best practices.
+        </p>
+        <p>
+          Launched all features using A/B implementation using Google Optimizie,
+          to take a data-driven approach towards UX.
         </p>
       </TimelineItem>
       <TimelineItem
-        key="001"
-        dateText="09/2019 – Present"
+        key="002"
+        dateText="01/2019 – 08/2019"
         style={{ color: "#e86971" }}
       >
         <h3>Software Engineer</h3>
-        <h4>@Wejo</h4>
+        <h4>@Wejo, San Jose, CA</h4>
+        <p>
+          Tech Stack:{" "}
+          <b>
+            ReactJS, NodeJS, ExpressJS, Javascript ES6, AWS Cognito, AWS
+            Amplify, Plotly.js, React Map GL,Bootstrap, Polyfills, Git, Enzyme,
+            Jest.
+          </b>
+        </p>
         <p>
           Designed and created visual insights using graphs and charts
           visualizing data aggregated into batches from a data stream.
@@ -68,6 +88,38 @@ const WorkExperience = () => {
           Lead the programming standards initiative to maintain code quality and
           best practices throughout the Wejo ecosystem
         </p>
+      </TimelineItem>
+      <TimelineItem
+        key="003"
+        dateText="07/2018 – 09/2018"
+        style={{ color: "#e86971" }}
+      >
+        <h3>Software Engineer</h3>
+        <h4>@SAP Ariba, Palo Alto, CA</h4>
+        <p>
+          Tech Stack:{" "}
+          <b>
+            Angular.js, Javascript ES6, TypeScript, GraphQL, Apollo, Karma,
+            Jasmine
+          </b>
+        </p>
+        <p>
+          Lead the User Tracking initiative that tracks user’s activities
+          site-wide.
+        </p>
+        <p>
+          Made a directive that is reusable and attachable to any HTML element,
+          dispatching an event using the above said angular HTTP service.
+        </p>
+        <p>
+          Built charts using D3 to visualize data, extracting data from a
+          GraphQL endpoint.
+        </p>
+        <p>
+          Made AJAX calls by subscribing to an Apollo observable to get data in
+          real time from GraphQL endpoint.
+        </p>
+        <p>Wrote unit tests, maintaining code coverage upwards of 85%.</p>
       </TimelineItem>
     </Timeline>
   );
